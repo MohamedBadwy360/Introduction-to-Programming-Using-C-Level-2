762abd71d563399b9ee9dec7fd5e87ea1143bb5a
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-10-05 18:35:18 +0300

    Automatic Variables

---
+++ 13. Automatic Variables/13. Automatic Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    auto x = 10;
+    auto y = 10.5;
+    auto z = "Mohamed";
+
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+}
+
