97ee00bda17a7c24ffebf23a3a5c79f347fbac55
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-10-05 12:21:39 +0300

    Function Declaration Vs Definition

---
+++ 07. Function Declaration Vs Definition/07. Function Declaration Vs Definition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// Funtion Declaration
+void sum(int, int);
+
+int main()
+{
+    sum(10, 20);
+}
+
+void sum(int number1, int number2)
+{
+    cout << number1 + number2 << endl;
+}
