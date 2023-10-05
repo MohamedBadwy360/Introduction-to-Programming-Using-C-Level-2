5c4d5b8bf0f4c0fde9f920df1de9025bc1e13195
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-10-05 11:58:30 +0300

    Bitwise And & Bitwise Or

---
+++ 06. Bitwise And & Bitwise Or/06. Bitwise And & Bitwise Or.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    std::cout << "And Result is: " << (12 && 25) << endl;   // = 1
+    std::cout << "Bitwise And Result is: " << (12 & 25) << endl;    // = 8
+    std::cout << "Or Result is: " << (12 || 25) << endl;   // = 1
+    std::cout << "Bitwise Or Result is: " << (12 | 25) << endl;    // = 1
+}
+
